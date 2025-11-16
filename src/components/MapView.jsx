d1d4dffccda4f7ebb82e86369c1796a57c4b66ce
Sarim <156024578+Saarim02@users.noreply.github.com>
2025-11-16 19:26:34 +0530

    Remove attribution from TileLayer component
    Removed attribution from TileLayer in MapView.

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -52,7 +52,6 @@ const MapView = ({ shipments }) => {
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {shipments.map((shipment) => (
